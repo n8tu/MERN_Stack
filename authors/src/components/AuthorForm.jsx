17a8533f9333b9a5737ed8644cf67e5523d3c0b8
n8tu <faisalz.nj@gmail.com>
2022-08-30 20:34:17 +0300

    Authors Assignment

--- authors/src/components/AuthorForm.jsx
+++ authors/src/components/AuthorForm.jsx
@@ -4,20 +4,50 @@ import {useHistory} from "react-router-dom";
 export default (props) => {
     const history = useHistory();
     const { initialName , onSubmitProps } = props;
-    const [name,setName] = useState(initialName);
+    const [name,setName] = useState({
+        value: initialName,
+        error: true,
+        message: "",
+    });
+
+    // Valid name before submit
+    const validName = (e) => {
+        let error = false;
+        let message = "";
+        if(e.target.value.length == 0){
+            error = true;
+            message = "Name is required!"
+        }
+        else if(e.target.value.length < 3){
+            error = true;
+            message = "Name must be at least 3 characters"
+        }
+        setName({
+            value: e.target.value,
+            error: error,
+            message: message
+        });
+    }
+
 
     const submitHandler = (e) => {
         e.preventDefault();
-        // send the data to create function throw onSubmit Props .
-        onSubmitProps({name})
+
+        // Check if all data has no validation errors
+        if([name.error].every((value) => value == false)){
+            // send the data to create function throw onSubmit Props .
+            onSubmitProps({name:name.value})
+        }
     }
+
 
     return(
         <div className="container w-50">
             <form  onSubmit={submitHandler} className={"border border-dark border-2 rounded p-3"}>
                 <div className="container">
                     <label htmlFor="author_name">Name:</label>
-                    <input onChange={(e) => setName(e.target.value)} value={name} type="text" className="form-control"/>
+                    <input onChange={validName} value={name.value} type="text" className="form-control"/>
+                    {name.error && (<span className="text-danger">{name.message}</span>)}
                 </div>
                 <div className="my-2">
                     <button type={"button"} className="btn btn-info mx-2 " onClick={() => history.push("/")}>Cancel</button>
